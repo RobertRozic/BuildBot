c88b1418531b706d46bb549dc6ca99e34e8f8217
Robert Rozic <rooobertinhooo@gmail.com>
2014-01-02 13:47:57 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -21,15 +21,12 @@ fi
 
 if [ $UPLOAD != "false" ]
 then
-  if [ "$REPO_BRANCH" = "jellybean" ] || [ $REPO_BRANCH = "cm-10.1" ] || [ $REPO_BRANCH = "cm-10.2" ];
-  then
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/jellybean/out/target/product/$DEVICE/$REPO_BRANCH*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
 
-  elif [ $REPO_BRANCH = "cm-11.0" ] || [ $REPO_BRANCH = "cm-11.0-test" ]
+  if [ $ROM_NAME = "cm_" ]
   then
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$REPO_BRANCH/out/target/product/$DEVICE/cm-11*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
 
   else
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$REPO_BRANCH/out/target/product/$DEVICE/$REPO_BRANCH*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
-  fi
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/$REPO_BRANCH*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
+
 fi
