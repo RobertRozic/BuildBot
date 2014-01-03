fcef3c8e349f51b82b04ecbeb1540ed7aa40008e
Robert Rozic <rooobertinhooo@gmail.com>
2014-01-03 16:35:24 +0100

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -15,10 +15,10 @@ then
 fi
 
 # Rom name
-if [ $REPO_BRANCH = "jellybean" ] || [ $REPO_BRANCH =~ "cm-" ]
+if [ $REPO_BRANCH = "cm-*" ]
 then
   export ROM_NAME="cm_"
-elif [ $REPO_BRANCH = "omni-4.3" ] || [ $REPO_BRANCH = "omni-4.4" ]
+elif [ $REPO_BRANCH = "omni-*" ]
 then
   export ROM_NAME="omni_"
 else
