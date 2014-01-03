1a0b5ee4d656290fcd97aee678df9db00938bc29
Robert Rozic <rooobertinhooo@gmail.com>
2014-01-03 14:58:34 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,8 +2,7 @@
 
 if [ $ROM_NAME = "cm_" ]
 then
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $PUBLIC
-elif [ $ROM_NAME = "omni_" ]
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
 then
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/omni-*.zip -f $FOLDER -d $DESC -pb  $PUBLIC
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/omni-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
 fi
