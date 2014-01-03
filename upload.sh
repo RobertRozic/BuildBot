63ffdb96b55ae57f33e9c9b099380ecf23f40e5a
Robert Rozic <rooobertinhooo@gmail.com>
2014-01-03 14:45:09 +0100

    Move variables to variables.sh

--- upload.sh
+++ upload.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-if [ $PUBLIC = "true" ]
-then
-  export DH_PUB=1
-else
-  export DH_PUB=0
-fi
-
-if [ -z "$DESC" ]
-then
-  DESC=None
-fi
-
 if [ $ROM_NAME = "cm_" ]
 then
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
-else
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/$REPO_BRANCH*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $PUBLIC
+elif [ $ROM_NAME = "omni_" ]
+then
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/omni-*.zip -f $FOLDER -d $DESC -pb  $PUBLIC
 fi
