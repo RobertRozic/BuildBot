3942c32d99c623b3e83466c5a0478d5154f5812b
Robert Rozic <rooobertinhooo@gmail.com>
2014-01-04 13:21:25 +0100

    Use rom name to fix ul_only

--- upload.sh
+++ upload.sh
@@ -2,13 +2,13 @@
 
 if [ $KERNEL_ONLY = "true" ]
 then
-  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/boot.img -f $FOLDER -d $DESC -pb  $DH_PUB
+  time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$ROM_NAME/out/target/product/$DEVICE/boot.img -f $FOLDER -d $DESC -pb  $DH_PUB
 else
   if [ $ROM_NAME = "cm" ]
   then
-    time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
+    time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$ROM_NAME/out/target/product/$DEVICE/cm-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
   elif [ $ROM_NAME = "omni" ]
   then
-    time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$JENKINS_BUILD_DIR/out/target/product/$DEVICE/omni-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
+    time devhost -u $DH_USER -p $DH_PASSWORD upload  $WORKSPACE/$ROM_NAME/out/target/product/$DEVICE/omni-*.zip -f $FOLDER -d $DESC -pb  $DH_PUB
   fi
 fi
